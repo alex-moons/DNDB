b1b448fc004ffd8a5edb5cdbec7d2ee0555deefb
PatoSantos <pato.santos10@gmail.com>
2022-09-13 18:36:55 -0500

    Se pueden conseguir Items de la API

--- dndb/src/App.js
+++ dndb/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Home from './pages/home';
+import ShowItems from './pages/items';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
   return (
     <div className="App">
-      <Home/>
+      <ShowItems/>
     </div>
   );
 }
