af6cd600f26e16058a73ec632f76245d39b93340
María Eugenia Ontiveros Bellés <a01197661@itesm.mx>
2022-09-14 19:16:14 -0500

    Update dashboard.js

--- dndb/src/components/pages/dashboard.js
+++ dndb/src/components/pages/dashboard.js
@@ -15,7 +15,6 @@ const Dashboard = () => {
                     Some quick example text to build on the card title and make up the
                     bulk of the card's content.
                     </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
                     </Card.Body>
                 </Card>
 
